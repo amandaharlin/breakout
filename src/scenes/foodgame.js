6bba7c8ab88b514497aa0676b62def0cf47d2272
Amanda Harlin <amanda.n.harlin@gmail.com>
2019-06-18 14:34:19 -0500

    removing some folders from CPA. figuring out hello world level code now

--- src/scenes/foodgame.js
+++ src/scenes/foodgame.js
@@ -1,13 +1,20 @@
 import Phaser from 'phaser/src/phaser.js';
 
+import seaTitle from '../assets/backgrounds/start/sea.png';
+import playerStill from '../assets/player-image.png';
 import { HEIGHT, SCALE, WIDTH } from '../config/constants';
 
 export default class FoodGame extends Phaser.Scene {
   constructor() {
     super({ key: 'FoodGame' });
   }
-  preload() {}
-  create() {}
+  preload() {
+    // this.load.image('sea-title', seaTitle);
+    this.load.image('player-still', playerStill);
+  }
+  create() {
+    this.add.image(100, 100, 'player-still', 0);
+  }
   update() {}
   render() {}
 }
